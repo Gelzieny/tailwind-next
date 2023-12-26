f6c579bd7dc9968c896fe5ecb163780b32b8c23f
Gelzieny R. Martins <gelzieny@gmail.com>
2023-12-25 22:57:19 -0300

    Pattern de Composição

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -3,18 +3,20 @@ import { Logo } from "./Logo";
 import { NavItem } from "./NavItem";
 import { UserdSpaceWidget } from "./UserdSpaceWidget";
 import { Profile } from "./Profile";
+import { InputControl, InputPrefix, InputRoot } from "../Input";
 
 export function Sidebar() {
   return (
     <aside className='border-r border-zinc-200 py-8 px-5 flex flex-col gap-6'>
       <Logo />
 
-      <div className="mx-1 flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2  shadow-sm">
-        <Search className="h-5 w-5 text-zinc-500" />
-        <input
-          placeholder="Search"
-          className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600" />
-      </div>
+      <InputRoot>
+        <InputPrefix>
+          <Search className="h-5 w-5 text-zinc-500" />
+        </InputPrefix>
+        <InputControl placeholder="Search" />
+      </InputRoot>
+
       <nav className="space-y-0.5">
         <NavItem title="Home" icon={Home} />
         <NavItem title="Dashboard" icon={BarChart} />
