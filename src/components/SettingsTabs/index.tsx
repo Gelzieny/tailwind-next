75e1fc094dba36d91282f941e0c98a8844afe1dc
Gelzieny R. Martins <gelzieny@gmail.com>
2024-01-20 20:30:48 -0300

    Criando Abas com Radix Tabs

--- src/components/SettingsTabs/index.tsx
+++ src/components/SettingsTabs/index.tsx
@@ -2,22 +2,25 @@
 
 import * as Tabs from '@radix-ui/react-tabs'
 import { TabsItem } from './TabsItem'
+import { useState } from 'react'
 
 export function SettingsTabs() {
+  const [currentTab, setCurrentTab] = useState('tab1')
+
   return (
     <>
-      <Tabs.Root>
+      <Tabs.Root value={currentTab} onValueChange={setCurrentTab}>
         <Tabs.List className='mt-6 flex w-full items-center gap-4 border-b border-zinc-200'>
-          <TabsItem value='tab1' title='My details' isSelected />
-          <TabsItem value='tab2' title='Profile' />
-          <TabsItem value='tab3' title='Password' />
-          <TabsItem value='tab4' title='Team' />
-          <TabsItem value='tab5' title='Plan' />
-          <TabsItem value='tab6' title='Billing' />
-          <TabsItem value='tab7' title='Email' />
-          <TabsItem value='tab8' title='Notifications' />
-          <TabsItem value='tab9' title='Integrations' />
-          <TabsItem value='tab10' title='API' />
+          <TabsItem value='tab1' title='My details' isSelected={currentTab === 'tab1'} />
+          <TabsItem value='tab2' title='Profile' isSelected={currentTab === 'tab2'} />
+          <TabsItem value='tab3' title='Password' isSelected={currentTab === 'tab3'} />
+          <TabsItem value='tab4' title='Team' isSelected={currentTab === 'tab4'} />
+          <TabsItem value='tab5' title='Plan' isSelected={currentTab === 'tab5'} />
+          <TabsItem value='tab6' title='Billing' isSelected={currentTab === 'tab6'} />
+          <TabsItem value='tab7' title='Email' isSelected={currentTab === 'tab7'} />
+          <TabsItem value='tab8' title='Notifications' isSelected={currentTab === 'tab8'} />
+          <TabsItem value='tab9' title='Integrations' isSelected={currentTab === 'tab9'} />
+          <TabsItem value='tab10' title='API' isSelected={currentTab === 'tab1'} />
         </Tabs.List>
       </Tabs.Root>
     </>
