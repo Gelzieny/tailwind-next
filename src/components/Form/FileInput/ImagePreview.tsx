fcd751b591e9190b76abc311efef22a9e89bf40e
Gelzieny R. Martins <gelzieny@gmail.com>
2024-01-20 21:50:08 -0300

    Componentizando File Input

---
+++ src/components/Form/FileInput/ImagePreview.tsx
@@ -0,0 +1,11 @@
+import { User } from "lucide-react";
+
+export function ImagePreview() {
+  return (
+    <div
+      className="bg-violet-50 flex w-16 h-16 items-center justify-center rounded-full"
+    >
+      <User className="w-8 h-8 text-violet-500" />
+    </div>
+  )
+}
