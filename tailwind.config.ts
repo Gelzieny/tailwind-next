1d24d21082d59671c9ac749c068563e93300c050
Gelzieny R. Martins <gelzieny@gmail.com>
2024-11-30 17:09:53 -0300

    Configurando projeto

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,14 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 export default {
-  content: [
-    "./src/**/*.tsx",
-  ],
+  content: ['./src/**/*.tsx'],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
       },
     },
   },
   plugins: [],
-} satisfies Config;
+} satisfies Config
