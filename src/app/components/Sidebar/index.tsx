3a4fa7e0e0461cf65ce47dc56c883a4d90215afa
Gelzieny R. Martins <gelzieny@gmail.com>
2024-11-30 18:38:09 -0300

    Widget de Espaço Usado

--- src/app/components/Sidebar/index.tsx
+++ src/app/components/Sidebar/index.tsx
@@ -1,10 +1,21 @@
-import { Search } from 'lucide-react'
+import {
+  BarChart,
+  CheckSquare,
+  Cog,
+  Flag,
+  Home,
+  LifeBuoy,
+  Search,
+  SquareStack,
+  Users,
+} from 'lucide-react'
 import { Logo } from './logo'
-import { MainNavigation } from './MainNavigation'
+import { NavItem } from './NavItem'
+import { UsedSpace } from './UsedSpace'
 
 export function Sidebar() {
   return (
-    <aside className="border-r border-zinc-200 px-5 py-8 space-y-6">
+    <aside className="border-r border-zinc-200 px-5 py-8 flex flex-col gap-6">
       <Logo />
 
       <div className="mx-1 flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm">
@@ -15,7 +26,17 @@ export function Sidebar() {
           className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600"
         />
       </div>
-      <MainNavigation />
+
+      <nav className="space-y-0.5">
+        <NavItem title="Home" icon={Home} />
+        <NavItem title="Dashboard" icon={BarChart} />
+        <NavItem title="Projects" icon={SquareStack} />
+        <NavItem title="Tasks" icon={CheckSquare} />
+        <NavItem title="Reporting" icon={Flag} />
+        <NavItem title="Users" icon={Users} />
+      </nav>
+
+      <UsedSpace />
     </aside>
   )
 }
