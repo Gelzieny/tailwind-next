3b9a1816f5add4e132de67cd4b939a08c54d608a
Gelzieny R. Martins <gelzieny@gmail.com>
2024-12-01 20:45:05 -0300

    Animações e Acessibilidade

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,6 +17,21 @@ export default {
           25: '#fcfaff',
         },
       },
+
+      maxWidth: {
+        app: '700px',
+      },
+
+      keyframes: {
+        slideDownAndFade: {
+          from: { opacity: 0, transform: 'translateY(-2px)' },
+          to: { opacity: 1, transform: 'translateY(0)' },
+        },
+      },
+      animation: {
+        slideDownAndFade:
+          'slideDownAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
+      },
     },
   },
   plugins: [],
