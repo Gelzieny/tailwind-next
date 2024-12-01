446c87afeee9087ed1c8dff566910c2c316458d9
Gelzieny R. Martins <gelzieny@gmail.com>
2024-12-01 17:56:39 -0300

    Criando Abas com Radix Tabs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-import { Sidebar } from './components/Sidebar'
+import { Sidebar } from '@/components/Sidebar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="antialiased">
       <body className={inter.className}>
         <div className="grid min-h-screen grid-cols-app">
           <Sidebar />
---
+++ src/components/SettingsTabs/index.tsx
@@ -0,0 +1,21 @@
+import * as Tabs from '@radix-ui/react-tabs'
+import { TabItem } from './TabItem'
+
+export function SettingsTabs() {
+  return (
+    <Tabs.Root>
+      <Tabs.List className="mt-6 flex w-full items-center gap-4 border-b border-zinc-200">
+        <TabItem value="tab1" title="My details" isSelected />
+        <TabItem value="tab2" title="Profile" />
+        <TabItem value="tab3" title="Password" />
+        <TabItem value="tab4" title="Team 4" />
+        <TabItem value="tab5" title="Plan" />
+        <TabItem value="tab6" title="Billing 2" />
+        <TabItem value="tab7" title="Email" />
+        <TabItem value="tab8" title="Notifications" />
+        <TabItem value="tab9" title="Integrations" />
+        <TabItem value="tab10" title="API" />
+      </Tabs.List>
+    </Tabs.Root>
+  )
+}
